7b82f7645418202dda1e1c2d00f92b0de746915f
denis <denismugambifelix@gmail.com>
2026-09-25 12:44:52 +0300

    chore: configure frontend application

---
+++ frontend/src/App.tsx
@@ -0,0 +1,32 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import AppLayout from './components/AppLayout'
+import ProtectedRoute from './components/ProtectedRoute'
+import Login from './pages/Login'
+import ProductManagement from './pages/ProductManagement'
+import Products from './pages/Products'
+import Signup from './pages/Signup'
+import Stats from './pages/Stats'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      <Route element={<ProtectedRoute />}>
+        <Route element={<AppLayout />}>
+          <Route path="/products" element={<Products />} />
+          <Route
+            path="/product-management"
+            element={<ProductManagement />}
+          />
+          <Route path="/stats" element={<Stats />} />
+        </Route>
+      </Route>
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  )
+}
+
+export default App
