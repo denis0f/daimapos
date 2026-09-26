a7cbad85bca1bb6e623736baac8c0559573ff34b
denis <denismugambifelix@gmail.com>
2026-09-26 17:12:04 +0300

    set the url to the frontend for the cors verification

--- backend/Program.cs
+++ backend/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("https://daimapos.onrender.com")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
